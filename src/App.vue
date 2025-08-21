b832b5995697fe264393399c87076f1f2747dd12
iliyasold <iliyasold@gmail.com>
2025-08-21 18:33:54 +0300

    Add Header with Score component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-  import Button from "../components/Button.vue";
+  import Button from "./components/Button.vue";
+  import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <main>
+  <Header />
+  <main class="main">
     <Button>Начать игру</Button>
   </main>
 </template>
